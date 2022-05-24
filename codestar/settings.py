7acd00f4523e3d9d43c887d126c43a2b013ec1e8
SteveMiller7 <info@millercreativedesign.com>
2022-05-24 14:34:46 +0000

    commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["codestar-ljblm.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['codestar-ljblm.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
