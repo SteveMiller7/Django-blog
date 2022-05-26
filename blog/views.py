a3027f82a784c8558b66847e0a46f4eb2bc2ef4f
SteveMiller7 <info@millercreativedesign.com>
2022-05-26 16:59:14 +0000

    added success message for posting a comment

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
-
+from django.contrib import messages
 
 
 class PostList(generic.ListView):
@@ -34,8 +34,9 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
-
+    
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -43,14 +44,14 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form = CommentForm(data=request.POST)    
-
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, 'You left a comment.')
         else:
             comment_form = CommentForm()
 
@@ -61,13 +62,15 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
 
+
 class PostLike(View):
-    def post(self, request, slug):
+    
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
